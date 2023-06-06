23fd6fbfdbc9a6c2584c950ae6eb48b8d65ce5f6
UmaMaheswaraReddy <matta.umamaheswarareddy@gmail.com>
2023-06-07 01:08:09 +0530

    Update bitXOR.rs

--- Experimenting/Bitwise XOR/bitXOR.rs
+++ Experimenting/Bitwise XOR/bitXOR.rs
@@ -1,24 +1,12 @@
 //Define the macro
-/*
-macro_rules! XOR {
-	($x:expr) => {
-		fn XOR() -> i32{
-			$x ^ 3
-		}
-	}
-}
-*/
+
 
 macro_rules! XOR {
 	($x:expr) => {
-		
 			$x ^ 3
-		
 	}
 }
 
-//Invoke the macro 
-//XOR!(4);
 
 //Calling the main exponent function
 fn main(){
